d27509665cc4eeff1de8983f8dc04876b81897b3
Chris <chris@aliquot.io>
2018-07-17 10:05:04 -0400

    Certifcate of Authority Extension

---
+++ extensions/dgx-cainstall/v1/cainstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Download and install a Certificate of Authority
+#
+# Note:
+#   This script will supports ubuntu. It has not been tested on other flavours
+#   of linux
+
+set -e
+
+sourceUri=$1
+targetPath=$2
+
+echo $(date) " - Starting Script"
+
+ curl -k $sourceUri -o $targetPath
+ update-ca-certificates
+
+echo $(date) " - Done"
